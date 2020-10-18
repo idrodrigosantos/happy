c6ba6a9a497772fa69aa12de4a1011783eb51227
Rodrigo Santos <imrodrigosantos@gmail.com>
2020-10-18 11:51:35 -0300

    Páginas alteradas para mostrar dados

--- public/scripts/page-orphanages.js
+++ public/scripts/page-orphanages.js
@@ -8,19 +8,33 @@ L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
 
 // Cria ícone
 const icon = L.icon({
-    iconUrl: 'public/images/map-marker.svg',
+    iconUrl: '/images/map-marker.svg',
     iconSize: [58, 68],
     iconAnchor: [29, 68],
     popupAnchor: [170, 2]
 });
 
-// Cria pop-up
-const popup = L.popup({
-    closeButton: false,
-    className: 'map-popup',
-    minWidth: 240,
-    minHeight: 240
-}).setContent('Orf. Esperança <a href="orphanage.html?id=1" class="choose-orphanage"><img src="public/images/arrow-white.svg"></a>');
+function addMarker({ id, name, lat, lng }) {
+    // Cria pop-up
+    const popup = L.popup({
+        closeButton: false,
+        className: 'map-popup',
+        minWidth: 240,
+        minHeight: 240
+    }).setContent(`${name} <a href="/orphanage?id=${id}"><img src="/images/arrow-white.svg"></a>`);
 
-// Pop-up no mapa
-L.marker([-27.222633, -49.6455874], { icon }).addTo(map).bindPopup(popup);
+    // Pop-up no mapa
+    L.marker([lat, lng], { icon }).addTo(map).bindPopup(popup);
+}
+
+const orphanagesSpan = document.querySelectorAll('.orphanages span');
+orphanagesSpan.forEach(span => {
+    const orphanage = {
+        id: span.dataset.id,
+        name: span.dataset.name,
+        lat: span.dataset.lat,
+        lng: span.dataset.lng
+    }
+
+    addMarker(orphanage);
+});
